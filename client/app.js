74c969abcc142d2f1dbd6c729916ea37e3c838d5
zhouxm@18.40 <zhouxm@jj.cn>
2019-03-26 17:28:15 +0800

    update

--- client/app.js
+++ client/app.js
@@ -1,41 +1,41 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/**
+ * Module dependencies.
+ */
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var config = require('./config').config;
+var express = require('express');
+var http = require('http');
+var model = require('./model').Model;
+var routes = require('./routes');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('tractor.view engine', 'pug');
+app.set('port', config.port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'secret'}));
+app.use(express.static(__dirname + '/public'));
+app.use(app.router);
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.get('/', routes.home);
+app.post('/register', routes.register);
+app.post('/login', routes.login);
+app.get('/logout', routes.logout);
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/rooms', routes.rooms);
+app.post('/join', routes.join);
+app.post('/leave', routes.leave);
+
+app.post('/service', routes.service);
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log("Express server listening on port " + app.get('port'));
 });
-
-module.exports = app;
