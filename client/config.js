672cd654b9f7da46024a7b71a68daa7e47614162
zhouxm@18.40 <zhouxm@jj.cn>
2019-03-26 17:09:32 +0800

    format project

---
+++ client/config.js
@@ -0,0 +1,17 @@
+// config.js
+
+const config = {
+    db: {
+        host: 'localhost',
+        port: 3306,
+        databases: 'demo',
+        username: 'localhost',
+        password: 'localhost',
+    },
+    app:{
+        masterKey:"tractor"
+    }
+};
+
+
+module.exports = config;
